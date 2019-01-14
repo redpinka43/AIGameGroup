24a74e9f54278e9f8801fa3691fc0cac4e825609
Lucia Martisovitsova <lucia.martisovitsova@gmail.com>
2019-01-14 14:37:44 -0500

    Here's the base game that we've got working. First real commit

---
+++ AIGame/Assets/Scripts/DialogHolder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is attached to any object that, when approached and you press SPACE,
+// it'll trigger the attached text.
+
+public class DialogHolder : MonoBehaviour {
+
+	private DialogueManager dMan;
+	public string[] dialogueLines;
+	// public int startLine;
+
+	// Use this for initialization
+	void Start () {
+		dMan = FindObjectOfType<DialogueManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	
+	void OnTriggerStay2D(Collider2D other) 
+	{
+		if(other.gameObject.name == "Player")
+		{
+			if(Input.GetKeyUp(KeyCode.Space))
+			{
+				Debug.Log("dialogue should happen now.");
+
+				if(!dMan.dialogActive)
+				{
+					dMan.dialogLines = dialogueLines;
+					dMan.currentLine = 0;
+					dMan.ShowDialogue();
+				}
+			}
+		}
+	}
+}
---
+++ AIGame/Assets/Scripts/DialogueNode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// This is a node which contains all the information for our dialogue line.
+// This corresponds to a single row in the spreadsheet.
+public class DialogueNode {
+
+	public string speaker;
+	public string text;
+	public int link;
+
+    public DialogueNode(string speaker, string text, int link) {
+        this.speaker = speaker;
+        this.text = text;
+        this.link = link;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
