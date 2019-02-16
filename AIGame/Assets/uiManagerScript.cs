7a50521a6fee010661239b6204de0d0eae1fb6a9
meaagaan <46685290+meaagaan@users.noreply.github.com>
2019-02-16 05:34:59 -0500

    First UI created: Start Menu
    includes "new game" and "load game" options, which currently only lead to "SampleScene" and "testScene2" respectively. Also uses placeholder artwork that will be replaced later

---
+++ AIGame/Assets/uiManagerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class uiManagerScript : MonoBehaviour
+{
+	
+	public void StartGame() 
+	{
+		SceneManager.LoadScene("SampleScene");
+	}
+	
+	public void LoadGame()
+	{
+		SceneManager.LoadScene("testScene2");
+	}
+
+}
