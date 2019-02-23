ea11e0ae78024ab3a96582e5fbdfc1bc349edd54
meaagaan <46685290+meaagaan@users.noreply.github.com>
2019-02-23 06:27:32 -0500

    Merged battleStart ui and battleFight ui into one scene

---
+++ AIGame/Assets/Scripts/petStat.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class petStat
+{
+    [SerializeField] private healthBarScript bar;
+	
+	[SerializeField] private float maxVal;
+	
+	[SerializeField] private float currentVal;
+	
+	public float CurrentVal
+	{
+		get
+		{
+			return currentVal;
+		}
+		
+		set
+		{
+			this.currentVal = value;
+			bar.Value = currentVal;
+		}
+	}
+	
+	public float MaxVal
+	{
+		get
+		{
+			return maxVal;
+		}
+		
+		set
+		{
+			this.maxVal = value;
+			bar.maxValue = maxVal;
+		}
+	}
+}
