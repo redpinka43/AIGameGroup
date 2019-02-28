f07705e6216abe7c5528da359e46c73da617263a
redpinka43 <lucia.martisovitsova@gmail.com>
2019-02-28 13:11:45 -0500

    Remade project for Unity Version 2018.2.19f1.

---
+++ AIGame/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public static CameraController instance = null;
+
+	public GameObject followTarget;
+	private Vector3 targetPos;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+		// Object singleton
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(transform.gameObject);
+		} else {
+			Destroy (gameObject);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+		transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+	}
+}
