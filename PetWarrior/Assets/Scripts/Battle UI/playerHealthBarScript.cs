b1aec04651e17f78d6fdf35dd936a935277518d7
chgranholm <42676474+chgranholm@users.noreply.github.com>
2019-03-03 16:14:11 -0500

    a couple of script changes

---
+++ PetWarrior/Assets/Scripts/Battle UI/playerHealthBarScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerHealthBarScript : MonoBehaviour
+{
+    [SerializeField] private float fillAmount;
+
+    [SerializeField] private Image filler;
+
+    private Pets playerPet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPet = GameObject.Find("playerPet").GetComponent<Pets>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleBar();
+    }
+
+    // updates how far the health bar should be filled
+    private void HandleBar()
+    {
+        // current health / health because this will give a decimal to the fill bar
+        // typecasted as floats to get decimals
+        fillAmount = (float)playerPet.currentHealth / (float)playerPet.health;
+        if (fillAmount != filler.fillAmount)
+        {
+            filler.fillAmount = fillAmount;
+        }
+    }
+}
--- PetWarrior/Assets/Scripts/Pet/petLevelUI.cs
+++ PetWarrior/Assets/Scripts/Pet/petLevelUI.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class petLevelUI : MonoBehaviour
 {
     Text txt;
-	int petLevel = 7;
+	int petLevel = 10;
 	
     // Start is called before the first frame update
     void Start()
