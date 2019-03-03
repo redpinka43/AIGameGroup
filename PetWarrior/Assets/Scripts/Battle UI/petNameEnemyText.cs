7d8c74bcd1602c4090923463b6b14d6409381bd7
chgranholm <42676474+chgranholm@users.noreply.github.com>
2019-03-03 16:14:11 -0500

    a couple of script changes

---
+++ PetWarrior/Assets/Scripts/Battle UI/petNameEnemyText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class petNameEnemyText : MonoBehaviour
+{
+    Text txt;
+    private Pets enemyPet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyPet = GameObject.Find("enemyPet").GetComponent<Pets>();
+        txt = GetComponentInChildren<Text>();
+        txt.text = enemyPet.name;
+    }
+}
