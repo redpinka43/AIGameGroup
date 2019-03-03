860aa48abcd07d0a723fa6fb6112c9226b3cd9a0
meaagaan <46685290+meaagaan@users.noreply.github.com>
2019-03-03 01:33:47 -0500

    added pause menu to basement scene

---
+++ PetWarrior/Assets/Scripts/UI related/pauseState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseState : MonoBehaviour 
+{
+
+	public GameObject pausePanel;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			pausePanel.SetActive(!pausePanel.activeSelf);
+		}
+	}
+}
