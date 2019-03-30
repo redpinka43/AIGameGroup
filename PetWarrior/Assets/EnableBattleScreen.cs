dafca4d71828e1ecf00fbe7181cd122d213e3583
chgranholm <42676474+chgranholm@users.noreply.github.com>
2019-03-30 18:49:13 -0400

    merge

---
+++ PetWarrior/Assets/EnableBattleScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableBattleScreen : MonoBehaviour {
+
+    private GameObject battle;
+    private GameObject mainCamera;
+    private GameObject battleCamera;
+
+    private PlayerController thePlayer;
+
+    // Use this for initialization
+    void Start () {
+        thePlayer = FindObjectOfType<PlayerController>();
+
+        battle = GameObject.Find("Battle");
+        mainCamera = GameObject.Find("Main Camera");
+        battleCamera = GameObject.Find("Battle Camera");
+ 
+        battle.SetActive(false);
+        battleCamera.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void startBattleScreen () {
+
+        mainCamera.SetActive(false);
+        battle.SetActive(true);
+        battleCamera.SetActive(true);
+        thePlayer.canMove = false;
+        
+    }
+}
---
+++ PetWarrior/Assets/PetEncounter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetEncounter : MonoBehaviour {
+
+    public Collider2D coll;
+    public EnableBattleScreen battleScreen;
+
+    private PlayerController thePlayer;
+    public int min, max;
+    // Use this for initialization
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerController>();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+
+            if(RNG(min, max) == 1)
+            {
+                Debug.Log("OKKKK");
+                battleScreen.startBattleScreen();
+            }
+        }
+        else
+            Debug.Log("Something else triggered");
+    }
+
+    int RNG(int min,int max)
+    {
+        int num;
+        num = Random.Range(min, max + 1);
+        Debug.Log("num is: "+num);
+        return num;
+    }
+}
