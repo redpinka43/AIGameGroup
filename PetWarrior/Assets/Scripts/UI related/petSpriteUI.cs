d0d9a773e6f44b620c9b9b1e32b432ec3a46c4c0
meaagaan <46685290+meaagaan@users.noreply.github.com>
2019-04-02 21:22:18 -0400

    M's temp

---
+++ PetWarrior/Assets/Scripts/UI related/petSpriteUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class petSpriteUI : MonoBehaviour {
+	
+	Image myImage;
+	public int i;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.Find("Player").GetComponent<Player>();
+		playerPet = player.playerPets[i]
+		
+		
+		myImage = GetComponent<Image>();
+        myImage.sprite = playerPet.image;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
