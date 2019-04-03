91663562a0ac649fe0f2a1bd3e838092f85c8b0d
chgranholm <42676474+chgranholm@users.noreply.github.com>
2019-04-02 22:53:41 -0400

    test update

---
+++ PetWarrior/Assets/Enemies.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour {
+
+    public List<Pets> enemyPets = new List<Pets>();
+    public List<string> items = new List<string>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ PetWarrior/Assets/getEnemyPet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getEnemyPet : MonoBehaviour {
+
+    private Enemies enemies;
+    public Pets enemyPet;
+    public Pets thisPet;
+
+    // Use this for initialization
+    void Start()
+    {
+        enemies = GameObject.Find("Enemy Pets").GetComponent<Enemies>();
+        thisPet = GameObject.Find("enemyPet").GetComponent<Pets>();
+        Debug.Log(thisPet.name);
+
+        enemyPet = enemies.enemyPets[Random.Range(0, 7)];
+        thisPet.name = enemyPet.name;
+        thisPet.animal = enemyPet.animal;
+        thisPet.image = enemyPet.image;
+        thisPet.species = enemyPet.species;
+        thisPet.health = enemyPet.health;
+        thisPet.currentHealth = enemyPet.currentHealth;
+
+        thisPet.maxAttack = enemyPet.maxAttack;
+        thisPet.attack = enemyPet.attack;
+
+        thisPet.maxDefense = enemyPet.maxDefense;
+        thisPet.defense = enemyPet.defense;
+
+        thisPet.special = enemyPet.special;
+        thisPet.speed = enemyPet.speed;
+        thisPet.moveNum = enemyPet.moveNum;
+        thisPet.moves = enemyPet.moves;
+        Debug.Log(thisPet.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisPet = enemyPet;
+
+    }
+}
