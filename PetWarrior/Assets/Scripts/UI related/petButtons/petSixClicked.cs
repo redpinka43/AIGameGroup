a24b243ee31f64a6286d6bfdd78f3aefa462cd89
meaagaan <46685290+meaagaan@users.noreply.github.com>
2019-04-28 17:32:56 -0400

    work on SwapPets script

---
+++ PetWarrior/Assets/Scripts/UI related/petButtons/petSixClicked.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// meaghan
+public class petSixClicked : MonoBehaviour {
+
+	public Button petSix;
+    public SwapPets SwapPets;
+    private Pets playerPet;
+    // Use this for initialization
+    void Start()
+    {
+        petSix.onClick.AddListener(petSix_Click);
+        SwapPets = GameObject.Find("BattleManager").GetComponent<SwapPets>();
+    }
+
+    public void petSix_Click()
+    {
+        SwapPets.pressedButton = 6;
+    }
+}
---
+++ PetWarrior/Assets/Scripts/UI related/petButtons/petThreeClicked.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// meaghan
+public class petThreeClicked : MonoBehaviour {
+
+	public Button petThree;
+    public SwapPets SwapPets;
+    private Pets playerPet;
+    // Use this for initialization
+    void Start()
+    {
+        petThree.onClick.AddListener(petThree_Click);
+        SwapPets = GameObject.Find("BattleManager").GetComponent<SwapPets>();
+    }
+
+    public void petThree_Click()
+    {
+        SwapPets.pressedButton = 3;
+    }
+}
