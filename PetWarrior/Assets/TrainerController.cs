315a859a84616923e3e43937030911016764f183
chgranholm <42676474+chgranholm@users.noreply.github.com>
2019-04-28 18:29:19 -0400

    Began working on the trainer party in the pet store

---
+++ PetWarrior/Assets/TrainerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainerController : MonoBehaviour {
+    public static TrainerController instance = null;
+
+    void Awake()
+    {
+        // Object singleton
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
